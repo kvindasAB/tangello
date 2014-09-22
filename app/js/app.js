19fadea789693e6b9e80efd0494f5a8f8820e749
Kibsaim Vindas <kvindas@alivebox.com>
2014-09-22 16:28:50 -0600

    Restructuring application. Integrating ui-router and ui-bootstrap.

---
+++ app/js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ui.bootstrap',
+  'ui.router',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/view1");
+}]);
