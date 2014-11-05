348a1c9470937178593804f8afca61cb530b6b57
Kibsaim Vindas <kvindas@alivebox.com>
2014-11-05 12:01:41 -0600

    Add new task functionality.

--- app/js/tasks/controllers/tasks-list-ctrl.js
+++ app/js/tasks/controllers/tasks-list-ctrl.js
@@ -3,11 +3,18 @@
 angular.module('tangello.tasks.controllers')
 
 .controller('TasksListController', ['$scope', function($scope) {
-  $scope.tasks = [
-    {title:"This is a task example"},
-    {title:"I need to buy the milk"},
-    {title:"We should render this on template"},
-    {title:"Have to pay some important bills"},
-    {title:"Need to learn angularjs"}
-  ];
+    $scope.newTaskTitle = "";
+
+    $scope.tasks = [
+      {title:"This is a task example"},
+      {title:"I need to buy the milk"},
+      {title:"We should render this on template"},
+      {title:"Have to pay some important bills"},
+      {title:"Need to learn angularjs"}
+    ];
+
+    $scope.addTask = function(title){
+      $scope.tasks.push({title: title});
+      $scope.newTaskTitle = "";
+    };
 }]);
